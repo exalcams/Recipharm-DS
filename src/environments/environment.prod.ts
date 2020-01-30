d8decc888937cf22a30f7679ac3e5946b2db1646
Ramanji <80186@EXALCA.CORP>
2020-01-30 12:19:47 +0530

    index Change

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,7 @@
 export const environment = {
     production: true,
     hmr       : false,
-    baseAddress : 'http://10.147.0.15:8090/v1/',
+    baseAddress : 'http://192.168.0.25:7181/v1/',
+    // baseAddress : 'http://10.147.0.15:7080/v1/',
     clientId : 'ngAuthApp'
 };
